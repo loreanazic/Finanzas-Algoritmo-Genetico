663bc1c1f0d16815e4d7f6c873ee3cb1a722f633
loreanazic <loreana.zambrano@unet.edu.ve>
2018-11-07 00:44:20 -0400

    Datos del algoritmo

--- src/finanzas/Finanzas.java
+++ src/finanzas/Finanzas.java
@@ -404,13 +404,13 @@ public class Finanzas {
             var++;
         } while (parada<700);
         
-        programa.imprimirPoblacion(" --------Poblacion final------- ",5);
+        programa.imprimirPoblacion(" --------Poblacion final------- ",10);
         //programa.imroimirValoresGrafica(" ----Rendimiento----Varianza--- ", 100);
         Scanner reader = new Scanner(System.in);
         int numero = 0;
         System.out.println("Introduce números de ejecucion del algoritmo");
         numero = reader.nextInt();
-        programa.escribirSalida(5,"corrida"+numero);
+        programa.escribirSalida(10,"corrida"+numero);
     }
     
 }
