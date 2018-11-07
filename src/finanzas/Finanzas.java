ee63ef5fa20d9de3142a5325b4361e5f86150eee
Yuleidy Valladares <yuleidyvalladares@gmail.com>
2018-11-07 00:54:05 -0400

    :octocat: Informe y eliminacion de comentarios

--- src/finanzas/Finanzas.java
+++ src/finanzas/Finanzas.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package finanzas;
 
 import java.io.FileWriter;
@@ -13,10 +8,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import jdk.nashorn.internal.runtime.ECMAErrors;
 
-/**
- *
- * @author loreanaisabel
- */
+
 public class Finanzas {
 
     Double [][] x; 
